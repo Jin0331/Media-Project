ac4b182ccedec99c891b49af406333f1a1b7071f
Jin0331 <sempre813@naver.com>
2024-02-05 15:11:46 +0900

    [Feat] URLSession을 통한 APIManager 추가

--- Media Project/Model/MediaSessionManager.swift
+++ Media Project/Model/MediaSessionManager.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class MediaSessionManager {
     
@@ -13,5 +14,74 @@ class MediaSessionManager {
     
     private init () {}
     
-    func 
+    //MARK: - Error 관련 Enum
+    enum APICError : Error {
+        case failedRequeset
+        case noData
+        case invalidResponse
+        case invalidData
+        case invalidDecodable
+    }
+    
+    //TODO: - Configuration URL세션으로 구성
+    func configuration<T: Decodable>(api : MediaAPI.Configuration, completionHandler : @escaping (T?, APICError?) -> Void) {
+        var url = URLRequest(url: api.endPoint)
+        url.httpMethod = "GET"
+        url.addValue(API.TMDBAPI, forHTTPHeaderField: "Authorization")
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            DispatchQueue.main.async {
+                
+                guard error == nil else {
+                    completionHandler(nil, .failedRequeset)
+                    return
+                }
+                
+                guard let data = data else {
+                    completionHandler(nil, .noData)
+                    return
+                }
+                
+                guard let response = response as? HTTPURLResponse else {
+                    completionHandler(nil, .invalidResponse)
+                    return
+                }
+                
+                guard response.statusCode == 200 else {
+                    completionHandler(nil, .invalidData)
+                    return
+                }
+                
+                do {
+                    let result = try JSONDecoder().decode(T.self, from: data)
+                    completionHandler(result, nil)
+                } catch {
+                    completionHandler(nil, .invalidDecodable)
+                }
+            }
+        }.resume()
+    }
+    
+    //TODO: - AF에서 URLSession으로 변경
+    func fetchTV<T : Decodable>(api : MediaAPI.TV, completionHandler : @escaping (T) -> Void) {
+        
+        AF.request(api.endPoint,
+                   method: MediaAPI.method,
+                   parameters: api.parameter,
+                   encoding: URLEncoding(destination: .queryString),
+                   headers: MediaAPI.header)
+        .responseDecodable(of: T.self) { response in
+            
+            switch response.result {
+            case .success(let success):
+                print("조회 성공")
+                
+                completionHandler(success)
+                
+            case .failure(let failure):
+                print(failure)
+            }
+        }
+    }
+    
 }
