318336c481b8f925c47925a9f8afceadb0cfb32b
Jin0331 <sempre813@naver.com>
2024-02-10 16:43:08 +0900

    [Feat] tableView Cell의 값 전달 기능 추가  - cellForRow을 통한 현재 index의 cell 추출  - closure를 이용한 값 전달.

--- Media Project/Controller/ProfileViewController.swift
+++ Media Project/Controller/ProfileViewController.swift
@@ -11,10 +11,13 @@ final class ProfileViewController: BaseViewController {
 
     let mainView = ProfileView()
     let profileSettingList = ["이름", "사용자 이름", "성별", "소개", "링크"]
-    var profileSettingDataList = ["이름", "사용자 이름", "성별", "소개", "링크"] {
-        didSet {
-            mainView.settingTableView.reloadData()
-        }
+    var profileSettingDataList = ["이름", "사용자 이름", "성별", "소개", "링크"]
+
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+//        mainView.settingTableView.reloadData()
+        print(#function)
     }
     
     override func loadView() {
@@ -50,7 +53,27 @@ extension ProfileViewController : UITableViewDelegate, UITableViewDataSource {
         cell.titleLabel.text = profileSettingList[indexPath.row]
         cell.titleTextField.placeholder = profileSettingDataList[indexPath.row]
         cell.titleTextField.setPlaceholderColor(.systemGray)
+        
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let currentCell = tableView.cellForRow(at: indexPath) as? Label_TextfieldTableViewCell else {
+            return
+        }
+        
+        let vc = ProfileDetailViewController()
+        vc.placeHolder = profileSettingDataList[indexPath.row]
+        vc.textField.text = currentCell.titleTextField.text
+        vc.nicknameSpace = { value in
+            print("안녕하세요")
+            print(value)
+            currentCell.titleTextField.text = value
+        }
+        
+        navigationController?.pushViewController(vc, animated: true)
+        
+        
     }
     
     
